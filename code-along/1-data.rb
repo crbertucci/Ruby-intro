dc491cdc4811ac4200e51b84aa36abdd5e417d12
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-01-23 14:53:34 +0000

    practice

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,54 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 1-data.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+# # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
 
-# Non-numbers
+# # Non-numbers
 
-# True, False
+# # True, False
 
-# Nothing
+# # Nothing
 
-# Variables
+# # Variables
 
-# Combine strings and variables
+# # Combine strings and variables
 
-# String manipulation
+# # String manipulation
+
+# # put the numnber 2 on screen
+# puts 2 
+
+# # put the number 5 on screen
+# puts 5
+
+# # put the sum of 5 and 2 on screen
+# puts 5+2
+
+# # Put tacos on screen 
+# puts "tacos"
+
+# # puts nil
+#  x = 2 
+#  y = 5
+
+# #put the value of x on screen
+
+# puts x
+
+# # put the value of y on screen
+
+# puts y
+
+# puts x * y 
+
+# store my favorite thing in memory
+favorite_thing = "tacos"
+
+# write favorire thing to the screen
+puts favorite_thing
+
+# write my favorite thing backwards on screen
+puts favorite_thing.reverse
