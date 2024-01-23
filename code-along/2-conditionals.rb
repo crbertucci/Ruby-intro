d8d8818f087b4c16491c35a93e853d1014ff1ef0
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-01-23 15:31:08 +0000

    ruby practice

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -13,3 +13,35 @@
 # Elsif Conditional Logic
 
 # Combining Expressions
+
+# #input
+# real_password = "tacos"
+# user_entered_password = "secret"
+
+# if real_password == user_entered_password
+# puts "You're in!"
+# else 
+#     puts "Ooops, that's the wrong password!"
+# end
+
+# our_team_score = 5
+# other_team_score = 5
+
+# if our_team_score > other_team_score
+#     puts "You Win!"
+# elsif our_team_score == other_team_score
+# puts "You tied!"
+# else
+#     puts "You lose!"
+
+# end
+temperature = 65
+precip = 0
+
+if temperature > 60 && precip == 0
+    puts "Nice!"
+elsif temperature>60 && precip>0
+    puts "said trombone"
+else
+    puts "it's gross"
+end
