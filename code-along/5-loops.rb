1ab0586287e960902226649974824edae77caac3
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-01-23 17:28:36 +0000

    ruby practice

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -8,4 +8,33 @@
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+for taco in tacos
+    puts "Eat #{taco} tacos!"
+end
+
+# #set an index variable
+# index = 0
+
+# #start a loop
+# loop do
+
+#     # break the loop if the number of elements is the same as the index
+#    if tacos.length == index
+#     break
+#    end
+
+#    #assign a variable to each taco
+
+#    taco = tacos[index]
+
+#    #print the output
+
+#     puts "EAT #{tacos[index]} tacos!"
+
+#     #increment the index
+
+# index = index + 1
+
+# end
