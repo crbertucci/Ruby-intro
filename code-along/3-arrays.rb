3d0f61788e714035804ad3fcd1b52ffa4f2068c8
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-01-23 16:01:16 +0000

    ruby practice

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -10,3 +10,31 @@
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+#create a list of Bens favorite foods
+bens_favorite_foods = ["kale", "sticks", "berries", "nuts"]
+
+
+#create list of our favorite foods
+favorite_foods = ["pizza", "ice cream"]
+
+#add another favorite food
+favorite_foods.push("dumplings")
+favorite_foods << "dumplings"
+
+our_favorite_foods = favorite_foods + bens_favorite_foods
+
+#write our faoire foods to screen
+# puts our_favorite_foods
+
+#create a shopping list
+
+shopping_list = [favorite_foods, bens_favorite_foods]
+
+#write bens #1 favoire foods
+
+# puts shopping_list[1][0]
+
+puts shopping_list
+
+puts "Number of favorite foods: #{our_favorite_foods.size}"
